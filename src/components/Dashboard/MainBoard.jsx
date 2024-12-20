fcbd2c1269087d684ad4d877338a61e0f2228369
Ope93yemi <adesolaprecious2015@gmail.com>
2024-12-20 16:25:37 +0100

    contacts section updated

--- src/components/Dashboard/MainBoard.jsx
+++ src/components/Dashboard/MainBoard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import MyApplication from "./Pages/MyApplication";
+import Contact from "./Pages/Contact";
 
 const MainBoard = ({ isSideDashOpen }) => {
   return (
@@ -13,8 +14,8 @@ const MainBoard = ({ isSideDashOpen }) => {
         {/* <Route path="overview" element={<Overview />} />
         <Route path="resume-builder" element={<ResumeBuilder />} /> */}
         <Route path="my-applications" element={<MyApplication />} />
-        {/* <Route path="contacts" element={<Contacts />} />
-        <Route path="support" element={<Support />} />
+        <Route path="contacts" element={<Contact />} />
+        {/* <Route path="support" element={<Support />} />
         <Route path="account" element={<Account />} /> */}
       </Routes>
     </div>
